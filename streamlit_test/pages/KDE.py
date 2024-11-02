7d492b6e482e0358c5b32280802232c1d909696b
TsaoHoward <cao.howard.ww@gmail.com>
2024-11-02 17:11:12 +0800

    1102-3

--- streamlit_test/pages/KDE.py
+++ streamlit_test/pages/KDE.py
@@ -21,24 +21,21 @@ year_max = int(df['交易年份'].max()) - 1
 selected_year = st.sidebar.slider("選擇交易年份", year_min, year_max, year_min)
 
 # Filter the data based on the selected year
-map_data = df[df['交易年份'] == selected_year][['經度', '緯度', 'Region']]
+map_data = df[df['交易年份'] == selected_year][['經度', '緯度', 'KDE_class']]
 map_data = map_data.rename(columns={'經度': 'longitude', '緯度': 'latitude'})
 
 # Define a function to map 'Region' to colors
 # Define a function to map 'Region' to colors for categories A-F
 def get_color(region):
     color_map = {
-        "A": [255, 0, 0],    # Red
-        "B": [0, 255, 0],    # Green
-        "C": [0, 0, 255],    # Blue
-        "D": [255, 255, 0],  # Yellow
-        "E": [255, 165, 0],  # Orange
-        "F": [128, 0, 128]   # Purple
-    }
+        1: [255, 0, 0],    # Red
+        2: [0, 255, 0],    # Green
+        3: [0, 0, 255],    # Blue
+        }
     return color_map.get(region, [0, 0, 0])  # Default to black if region is not in A-F
 
 # Apply the color function to the data
-map_data['color'] = map_data['Region'].apply(get_color)
+map_data['color'] = map_data['KDE_class'].apply(get_color)
 
 # Create PyDeck layer using the map data with colors based on 'Region'
 selected_layers = [
--- streamlit_test/pages/LSTM.py
+++ streamlit_test/pages/LSTM.py
@@ -1,18 +1,26 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-
+import numpy as np
+# pip install streamlit-folium
+import folium
+from streamlit_folium import folium_static
+from streamlit_folium import st_folium
 from menu import menu
 
 menu()
 
+m = folium.Map(location=[23.13, 120.312480], zoom_start=10)
 
-import streamlit as st
-import numpy as np
-import pandas as pd
+# Use st_folium to display the map and get the click data
+map_data = st_folium(m, width=700, height=500)
 
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+# Check if the map was clicked
+if map_data['last_clicked']:
+    clicked_lat = map_data['last_clicked']['lat']
+    clicked_lon = map_data['last_clicked']['lng']
+    st.write(f"點擊位置的經緯度: {clicked_lat}, {clicked_lon}")
 
-st.map(map_data)
+with st.expander("南科聚落", expanded=True):
+    st.page_link("pages/DBSCAN.py", label="聚落分布", icon="🔐")
+    st.page_link("pages/KDE.py", label="交易密度", icon="👤")
